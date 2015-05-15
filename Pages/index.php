ef72e24c4034ce6eaeea3102ff46ddf500738a0b
Jourdan Wallace <jaywallace713@gmail.com>
2015-05-15 09:19:25 -0700

    Got a little bit of coding done today, but that is about it

--- Pages/index.php
+++ Pages/index.php
@@ -1,3 +1,70 @@
+<!DOCTYPE HTML>
 <?php
+require_once("");
+?>
+<html>
+    <head>
+        <title>melonJS Template</title>
+        <link rel="stylesheet" type="text/css" media="screen" href="index.css">
+        <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <link rel="apple-touch-icon" href="icons/touch-icon-iphone-60x60.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="icons/touch-icon-ipad-76x76.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="icons/touch-icon-iphone-retina-120x120.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="icons/touch-icon-ipad-retina-152x152.png">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    </head>
+    <body>
+        <title>JQuery Page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="css/custom-style.css">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <!--The code necessary to make the accordion work-->
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+    </head>
+<body>
+    <!--The image I am using-->
+    <img id="logo" height="200" width="200" src="http://fc01.deviantart.net/fs70/f/2013/213/f/3/scandroid___skull_robot_logo_by_silentdan297-d6g7hb9.png" alt="">
+    <div class="p">
+        <!--The text I am using in the first paragraph-->
+        <h1>Third milestone hover</h1>
+        <p>Hi my name is Jourdan and this is my new portfolio project! </p>
+        <p>I am still a freshman at PHS and I am still in the App Academy.</p>
+    </div>
+    <div class="p">
+        <!--The text I am using in the middle paragraph-->
+        <p>I am still a student athlete playing tennis and basketball.</p>
+        <p>We are now moving into JQuery and it is really getting on my nerves.</p>     
+    </div>
+
+    <div class="p">
+        <!--The text I am using for the last paragraph-->
+        <p>This project is a little bit more confusing then the others but it's fun.</p>
+        <p>Almost done with the project, just trying to make it look cool!</p>     
+    </div>
+
+    <div><h2>Playing with Text</h2></div>
+    <div>
+        <!--The tag I have decided to use-->
+        <h3 id="h3Tag">H3 Tag</h3>
+    </div>
+
+    <div>
+        <!--The links to the other pages I have made-->
+        <a class="list-group-item" href="index.html">About Me</a>
+        <a class="list-group-item" href="projects.html">Projects</a>
+        <a class="list-group-item" href="technologies.html">Technologies</a>
+    </div>
+</body>
+</html>
+
 
 
--- index.php
+++ index.php
@@ -1,25 +1,34 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
+        <title>About Me</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="css/custom-style.css">
+         <link rel="apple-touch-icon" href="icons/touch-icon-iphone-60x60.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="icons/touch-icon-ipad-76x76.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="icons/touch-icon-iphone-retina-120x120.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="icons/touch-icon-ipad-retina-152x152.png">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
     </head>
     <body>
-        <?php
-    //Taking the information from these pages and putting them into post and created the user 
-    //require_once(__DIR__ . "/controller/login-verify.php");
-    //require_once(__DIR__ . "/view/header.php");
-    //if(authenticateUser()) {
-        //require_once(__DIR__ . "/view/navigation.php");
-    //}
-    //require_once(__DIR__ . "/controller/create-db.php");
-    //require_once(__DIR__ . "/view/footer.php");
-    //require_once(__DIR__ . "/controller/read-posts.php");
-?>
+        <div class="jumbotron">
+  <h1>TENNIS ROCKS</h1>
+  <p>Tennis Website, click the link below.</p>
+  <p><a href="http://www.tennis.com/" class="btn btn-primary btn-lg" role="button">Tennis</a></p>
+  </div>
+        <p>About Me</p>
+        <p>I am Jourdan Wallace, and this is my final project!</p>
+        <p>Behind this page are some of the best tennis players in the world </p>
+        <div class="col-xs-2">
+            <nav class="list-group">
+            </nav>
+        </div>
     </body>
 </html>
+
+
+
