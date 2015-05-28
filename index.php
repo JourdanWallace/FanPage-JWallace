a2bbd8169f8d08add07db1ff9f8b71df55469f07
Jourdan Wallace <jaywallace713@gmail.com>
2015-05-28 12:01:15 -0700

    Sorry this is late

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+        <!--Title of document-->
         <title>About Me</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,6 +15,7 @@
         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script> 
         <script type="text/JavaScript">
+            //Information for the rating system
             $(document).ready(function (){ 
                 $('#ratingdiv .undone').click(function(){ 
                     var div = '#ratingdiv';	
@@ -29,19 +31,22 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
     </head>
     <body>
+        <!--jumbotron info-->
         <div class="jumbotron">
+            <!--jumbotron title-->
   <h1>TENNIS ROCKS</h1>
+  <!--links to different pages-->
   <p>Tennis Website, click the link below.</p>
   <p><a href="http://www.tennis.com/" class="btn btn-primary btn-lg" role="button">Tennis</a></p>
   <p><a href="pages/SerenaWilliams.html" class="btn btn-primary btn-lg" role="button">Serena</a></p>
   <p><a href="pages/VenusWilliams.html" class="btn btn-primary btn-lg" role="button">Venus</a></p>
   <p><a href="pages/MariaSharapova.html" class="btn btn-primary btn-lg" role="button">Maria</a></p>
-  <p><a href="pages/RafaelNadal.html" class="btn btn-primary btn-lg" role="button">Rafael</a></p>
-  <p><a href="pages/NovakDjokovic.html" class="btn btn-primary btn-lg" role="button">Novak</a></p>
-  <p><a href="pages/RogerFederer.html" class="btn btn-primary btn-lg" role="button">Roger</a></p>
+  <p><a href="pages/Rafael.html" class="btn btn-primary btn-lg" role="button">Rafael</a></p>
+  <p><a href="pages/Novak.html" class="btn btn-primary btn-lg" role="button">Novak</a></p>
+  <p><a href="pages/Roger.html" class="btn btn-primary btn-lg" role="button">Roger</a></p>
 
   </div>
-        
+        <!--quick little bio-->
         <p>About Me</p>
         <p>I am Jourdan Wallace, and this is my final project!</p>
         <p>Behind this page are some of the best tennis players in the world </p>
